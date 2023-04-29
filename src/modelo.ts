c8f642d590459a1251add2d4682c007bb9133722
Braulio Diez Botella <braulio.diez@gmail.com>
2023-04-29 18:10:25 +0200

    seed+ model

---
+++ src/modelo.ts
@@ -0,0 +1,17 @@
+type teclas = "azul" | "rojo" | "verde" | "amarillo";
+
+type estadoPartida = "no_arrancada" | "jugando" | "game_over";
+
+export interface Partida {
+  ronda: number;
+  notas: teclas[];
+  notasUsuario: teclas[];
+  estado: estadoPartida;
+}
+
+export const crearPartidaInicial = () => ({
+  ronda: 0,
+  notas: [],
+  notasUsuario: [],
+  estado: "no_arrancada",
+});
